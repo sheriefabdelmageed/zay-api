d55799b8582282b57b63857baf83314110cd2284
sherief abdelmageed <sheriefabdelmageed@gmail.com>
2020-03-29 20:48:10 +0200

    set port

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/files/:img", (req, res) => {
   } catch (error) {}
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("server is started!");
 });
